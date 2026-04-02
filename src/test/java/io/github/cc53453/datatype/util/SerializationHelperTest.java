fbf24905c4f4caf9f62ebca660623e7ebb6b6c0f
chen <3057698192@qq.com>
2026-04-03 07:04:30 +0800

    增加单元测试

---
+++ src/test/java/io/github/cc53453/datatype/util/SerializationHelperTest.java
@@ -0,0 +1,20 @@
+package io.github.cc53453.datatype.util;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SerializationHelperTest {
+    @Test
+    void test() throws IOException {
+    	SerializationHelper<String> helper = new SerializationHelper<>();
+    	helper.serializeByJson("coaihcdoac", "test-out/serialize-test.txt");
+    	Assertions.assertEquals("coaihcdoac", 
+    			helper.deserializeByJson("test-out/serialize-test.txt"));
+    }
+
+}
