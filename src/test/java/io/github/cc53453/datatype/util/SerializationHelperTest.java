13127fe7f47f584f4e730c6c9a598aa50f6bb36e
chen <3057698192@qq.com>
2026-04-05 15:29:40 +0800

    sonar漏洞修复

--- src/test/java/io/github/cc53453/datatype/util/SerializationHelperTest.java
+++ src/test/java/io/github/cc53453/datatype/util/SerializationHelperTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class SerializationHelperTest {
+class SerializationHelperTest {
     @Test
     void test() throws IOException {
     	SerializationHelper<String> helper = new SerializationHelper<>();
